9affc26b148b2bd98ed6d178dc08361997798bcb
Martin Bednar <mbednar@vendavo.com>
2018-03-28 12:30:21 +0200

    Sample benchmark

---
+++ src/jmh/java/com/github/xxbedy/jmh/SimpleJmhBenchmark.java
@@ -0,0 +1,18 @@
+package com.github.xxbedy.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+@State(Scope.Benchmark)
+public class SimpleJmhBenchmark {
+
+    @Benchmark
+    public void stringConcat() {
+    	String foo = "foo";
+    	String bar = "bar";
+    	String concat = foo + bar;
+    }
+
+}
